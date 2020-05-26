b9617650259863601373e10e6c46f3854c3d54e2
Håkan Nylén <hakan@dun.se>
2020-05-26 14:18:11 +0200

    removed testfile

--- test.cr
+++
@@ -1,7 +0,0 @@
-require "./src/referer-parser.cr"
-
-
-parser = RefererParser::Parser.new
-result = parser.parse("http://www.google.com/search?q=gateway+oracle+cards+denise+linn&hl=en&client=safari")
-
-pp! result
